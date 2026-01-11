916dd8ed5133b45429bddfa6418ad80498b60cf0
Sangheon Lee <wwdbsh@gmail.com>
2026-01-11 11:35:35 +0900

    first commit

---
+++ src/search/S2_1260.java
@@ -0,0 +1,13 @@
+package search;
+
+import java.io.*;
+import java.util.*;
+
+public class S2_1260 {
+    public static void main(String[] args) throws IOException {
+        
+        System.setIn(new FileInputStream("inputs/temp.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
